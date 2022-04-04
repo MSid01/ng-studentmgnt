e1ec5ed6bf85176563793b7333a9bab0ae51a8ec
Siddhesh <mungekarsiddhesh2001@gmail.com>
2022-04-04 21:55:14 +0530

    files changed

--- student-management-sys/studentcontroller.js
+++ student-management-sys/studentcontroller.js
@@ -1,68 +1,71 @@
-angular
-	.module("studentApp")
-	.controller("StudentController", StudentController);
+// StudentController.js
+angular.module("studentApp").controller("StudentController", StudentController);
 
-StudentController.$inject[
-						  'StudentService'
-						];
+StudentController.$inject["StudentService"];
 
-function StudentController(StudentService){
-	var vm = this;
-	vm.students = StudentService.getStudents();
-	vm.orderBy = orderBy;
-	vm.deleteStudent = deleteStudent;
-	vm.showStatus = false;
-	vm.doShow = doShow;
+function StudentController(StudentService) {
+  var vm = this;
+  vm.students = StudentService.getStudents();
+  vm.orderBy = orderBy;
+  vm.deleteStudent = deleteStudent;
+  vm.showStatus = false;
+  vm.doShow = doShow;
 
-	vm.addStudent = addStudent;
-	vm.editStudent = editStudent;
+  vm.addStudent = addStudent;
+  vm.editStudent = editStudent;
 
-	vm.editorEnabled = [];
-	vm.enableEditor = enableEditor;
-	vm.disableEditor = disableEditor;
+  vm.editorEnabled = [];
+  vm.enableEditor = enableEditor;
+  vm.disableEditor = disableEditor;
 
-	function orderBy(myOrder){
-		vm.ordering = myOrder;
-	} 
+  function orderBy(myOrder) {
+    vm.ordering = myOrder;
+  }
 
-	function deleteStudent(id){
-		StudentService.deleteStudent(id);
-	}
+  function deleteStudent(id) {
+    StudentService.deleteStudent(id);
+  }
 
-	function doShow(){
-		vm.showStatus = true;
-	}
+  function doShow() {
+    vm.showStatus = true;
+  }
 
-	function addStudent(){
-		var maxId = StudentService.maxId();
-		vm.student = {
-			id: maxId + 1,
-			name: vm.sname,
-			address: vm.saddress,
-			class: vm.sclass,
-			mobile: vm.smobile
-		};
-		StudentService.addStudent(vm.student);
-	}
+  function addStudent() {
+    var maxId = StudentService.maxId();
+    vm.student = {
+      id: maxId + 1,
+      name: vm.sname,
+      address: vm.saddress,
+      branch: vm.sbranch,
+      mobile: vm.smobile,
+    };
+    StudentService.addStudent(vm.student);
+  }
 
-	function editStudent($index, editId, editName, editAddress, editClass, editMobile){
-		vm.student = {
-			id: editId,
-			name: editName,
-			address: editAddress,
-			class: editClass,
-			mobile: editMobile
-		};
-		StudentService.editStudent(vm.student);
-		vm.disableEditor($index);
-	}
+  function editStudent(
+    $index,
+    editId,
+    editName,
+    editAddress,
+    editBranch,
+    editMobile
+  ) {
+    vm.student = {
+      id: editId,
+      name: editName,
+      address: editAddress,
+      branch: editBranch,
+      mobile: editMobile,
+    };
+    StudentService.editStudent(vm.student);
+    vm.disableEditor($index);
+  }
 
-	function enableEditor(index){
-		vm.editorEnabled[index] = true;
-	}
+  function enableEditor(index) {
+    vm.editorEnabled[index] = true;
+  }
 
-	function disableEditor(index){
-		vm.editorEnabled[index] = false;
-	}
-
+  function disableEditor(index) {
+    vm.editorEnabled[index] = false;
+  }
 }
--- student-management-sys/studentdirective.js
+++ student-management-sys/studentdirective.js
@@ -1,23 +1,24 @@
+// studetdirective.js
 var app = angular.module("studentApp");
 
-	app.directive("studentSearch", studentSearch);
-	app.directive("studentDisplay", studentDisplay);
-	app.directive("addStudent", addStudent);
+app.directive("studentSearch", studentSearch);
+app.directive("studentDisplay", studentDisplay);
+app.directive("addStudent", addStudent);
 
-function studentSearch(){
-	return {
-		templateUrl: "studentsearch.htm"
-	};
+function studentSearch() {
+  return {
+    templateUrl: "studentsearch.htm",
+  };
 }
 
-function studentDisplay(){
-	return {
-		templateUrl: "studentdisplay.htm"
-	};
+function studentDisplay() {
+  return {
+    templateUrl: "studentdisplay.htm",
+  };
 }
 
-function addStudent(){
-	return {
-		templateUrl: "addStudent.htm"
-	};
+function addStudent() {
+  return {
+    templateUrl: "addStudent.htm",
+  };
 }
